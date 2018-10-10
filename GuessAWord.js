af1c671c09f505ccc13f8649c983a94aeab5f04b
McFkCoding <Mcfkcoding@gmail.com>
2018-10-10 10:16:28 +0300

    re-formated

--- GuessAWord.js
+++ GuessAWord.js
@@ -33,38 +33,38 @@ process.stdin.on( "data", function( input_from_user )
 		for ( var character_index in word_to_be_guessed ) 
 		{
          		if ( word_to_be_guessed[ character_index ] == player_input[ 0 ] ) 
-			{
+				{
             			guessed_characters[ character_index ] = player_input[ 0 ] ;
          		}
-      		}
-      		if ( guessed_characters.indexOf( "-" ) == -1 ) 
+      	}
+      	if ( guessed_characters.indexOf( "-" ) == -1 ) 
 		{
          		process.stdout.write( " " + guessed_characters.join( "" ) + "  Congratulations! \n\n" ) ;
 			process.stdout.write( "\n Number of guesses : " + GuCou + "\n");
          		process.stdout.write("Do you wanna play more (Y/N) ?");
 			check = true;
 			record[index].guesses++;
-      		}
+      	}
 		if (check == false) 
 		{
 			process.stdout.write( " " + guessed_characters.join( "" ) + "  Give a character or word: " ) ;
 		}
 	} else  if ( player_input.length > 1 && check == false ) 
-		{
-      			if ( player_input == word_to_be_guessed  ) 
 			{
+      			if ( player_input == word_to_be_guessed  ) 
+				{
          			process.stdout.write( "\n Congratulations! \n\n" ) ;
-				process.stdout.write( "\n Number of guesses : " + GuCou +  "\n");
+					process.stdout.write( "\n Number of guesses : " + GuCou +  "\n");
          			check = true;
-				process.stdout.write("Do you wanna play more (Y/N) ?");
+					process.stdout.write("Do you wanna play more (Y/N) ?");
       			}
    	} else 	if ( player_input.length < 1 && check == false )
-		{	
-      				process.stdout.write( " \n Game interrupted. \n\n" ) ;
+			{	
+      			process.stdout.write( " \n Game interrupted. \n\n" ) ;
 				check = true;
 				process.stdout.write("Do you wanna play more (Y/N) ?");
    	} else 	if ( player_input == "Y" ) 
-		{
+			{
 				process.stdout.write("Let da game begin again, rules r the same ;) \n");
 				check = false;
 				index = Math.floor( Math.random() * wordPool.length );
@@ -77,21 +77,18 @@ process.stdin.on( "data", function( input_from_user )
 				process.stdout.write( " " + guessed_characters.join( "" ) + "  Give a character or word: " ) ;
 
 	} else 
-	{
-		record.sort(function(a, b) 
 		{
-			return ((a.guesses > b.guesses) ? -1 : ((a.guesses == b.guesses) ? 0 : 1));
-		});
-
-		process.stdout.write("Played Words" + "\t | \t" + "Guesses\n");
-		for ( var i = 0; i < record.length; i++ ) 
-		{
-			process.stdout.write(record[i].playedWords + "\t\t | \t" + record[i].guesses + "\n");
-		}
-
-		process.stdout.write("\n");
-		process.exit();
+			record.sort(function(a, b) 
+			{
+				return ((a.guesses > b.guesses) ? -1 : ((a.guesses == b.guesses) ? 0 : 1));
+			});
+			process.stdout.write("Played Words" + "\t | \t" + "Guesses\n");
+			for ( var i = 0; i < record.length; i++ ) 
+			{
+				process.stdout.write(record[i].playedWords + "\t\t | \t" + record[i].guesses + "\n");
+			}
+			process.stdout.write("\n");
+			process.exit();
 	}
-
 } ) ;
 
